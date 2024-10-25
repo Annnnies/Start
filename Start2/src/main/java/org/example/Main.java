3df720faeba21ceb9f9cc353d90574c6e77d003a
Annnnies <sawatdiikha7@gmail.com>
2024-10-25 17:00:22 +0300

    Add files via upload

---
+++ Start2/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("What is the fuel cost of the car per 100 km?");
+        var fuel = scanner.nextDouble();
+        System.out.println("Fuel cost is " + fuel);
+
+        while (true) {
+            System.out.println("How many kilometers?");
+            var distance = scanner.nextDouble();
+
+            if (distance == 0)
+                break;
+
+            var result = fuel * distance / 100;
+            System.out.println("Result = " + result);
+        }
+    }
+}
